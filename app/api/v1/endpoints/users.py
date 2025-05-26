b899fa7f40ccee2146efea1350d540df9acc8479
GonzaloVegaRamos <brevavega@gmail.com>
2025-05-27 01:46:32 +0200

    vjghhgc

--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -109,6 +109,7 @@ async def google_register(user: schemas.GoogleUserCreate):
             "auth_id": user.id,
             "email": user.email,
             "full_name": user.full_name,
+            "avatar_url": user.avatar_url,
             "username": username,  # Campo requerido
             "edad": 18,  # Valor por defecto
             "gender": "other",  # Valor por defecto
